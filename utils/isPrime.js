75403ec864cf83437600dedfa84dcaad7050fc89
Joergen Larsen Tjoennteig <jorgenlt@outlook.com>
2025-02-25 10:02:13 +0800

    utils

---
+++ utils/isPrime.js
@@ -0,0 +1,13 @@
+const isPrime = (number) => {
+  if (number <= 1) return false;
+  if (number === 2) return true;
+  if (number % 2 === 0) return false;
+
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    if (number % i === 0) return false;
+  }
+
+  return true;
+};
+
+export default isPrime;
